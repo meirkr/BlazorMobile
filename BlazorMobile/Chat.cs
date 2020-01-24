c1f059fb48d484d4a23e8d29d8983e317e41f2c5
Meir Kriheli <meirkr@gmail.com>
2020-01-24 03:00:49 +0200

    try the old signalr

--- BlazorMobile/Chat.cs
+++ BlazorMobile/Chat.cs
@@ -1,17 +1,50 @@
-﻿
-
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
 
 namespace BlazorMobile
 {
     public partial class Chat
     {
+        private HubConnection _hubConnection;
+        string _connectId;
+        private string _lastMsg;
 
-        int count = 100000;
-
-        void IncrementCount()
+        //public Chat(IHubConnectionBuilder hubConnectionBuilder)
+        //{
+        //    this.hubConnectionBuilder = hubConnectionBuilder;
+        //}
+        protected async override Task OnInitializedAsync()
         {
-            count--;
+            const string url = "ws://meirkr.com/chat";
+            _hubConnection = new HubConnection(url);
+
+
+            //_hubConnection.Reconnected += (connected =>
+            //    {
+            //        _connectId = connected;
+            //        base.StateHasChanged();
+            //        return Task.CompletedTask;
+            //    });
+
+
+            //    On<string>("sendToAll", msg =>
+            //{
+            //    _lastMsg = msg;
+            //    base.StateHasChanged();
+            //});
+            _hubConnection.Received +=
+                (msg =>
+                {
+                    this._lastMsg = msg;
+                });
+
+            await _hubConnection.Start();
+
+            await base.OnInitializedAsync();
         }
+
+
     }
 
 }
